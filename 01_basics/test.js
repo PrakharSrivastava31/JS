e8e1e813d569b302f37fd52a29d14273a4dd2198
Prakhar Srivastava <prakhar.sri3103@gmail.com>
2024-12-23 22:40:50 +0530

    basic_1
    part-1

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
 console.log("Prakhar")
+console.log("beautiful")
+
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) we are using node.js not the browser
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
